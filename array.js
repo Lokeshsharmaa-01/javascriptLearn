9840f0d261211f47d2f05e1e5bdbd2c9c17ff238
Lokeshsharmaa-01 <iamlokesh2024@gmail.com>
2024-01-26 21:55:02 +0530

    adding some more concepts

---
+++ array.js
@@ -0,0 +1,13 @@
+let myarr = [1,34,5,6,7,7]
+// console.log(myarr.length)
+myarr[6] = "hi"
+// console.log(myarr)
+
+
+let m1 = ["hi","hello","ji"]
+// console.log(m1.slice(1,3))
+console.log(myarr.concat(m1))
+
+// console.log(m1.splice(1,4))
+// console.log(myarr)
+
