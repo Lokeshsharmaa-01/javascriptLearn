037a94ddfeb9ec651caaa909150c309df0ea030e
Lokeshsharmaa-01 <iamlokesh2024@gmail.com>
2024-03-03 21:36:00 +0530

    Promise learn

--- promises.js
+++ promises.js
@@ -1,18 +1,68 @@
-// const promiseOne = new Promise(function (resolve,reject){
-//     console.log("async one is complete")
-//     resolve()
+// const promiseOne = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         console.log('Async task is  complete')
+//         resolve()
+//     },1000)
 // })
 
 // promiseOne.then(function(){
-//     console.log('abhi hum chalte hain')
+//     console.log('Promise is consumed')
 // })
 
 
-new Promise(function(resolve,reject){
+// const promiseThree = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         reject({username: "lokesh sharma" , email: "iamlokeshsharmaa@gmail.com"})
+//     },1000)
+// })
+
+// promiseThree.then(function(user){
+//     console.log(user)
+// })
+
+
+
+// const promisefour = new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         let error = false;
+//         if(!error){
+//             resolve({username: "loeksh",password: "1245"})
+//         } else {
+//             reject ('Error, something went wrong')
+//         }
+//     },1000)
+// })
+
+// promisefour.then(function(user){
+//     console.log(user)
+//     return user.username
+// }).then((username)=>{
+//     console.log(username)
+// }).catch((error)=>{
+//     console.log(error)
+// })
+
+
+const promiseFive = new Promise(function(resolve,reject){
     setTimeout(function(){
-        console.log('async 2 task')
-        resolve();
-    })
-}).then(function(){
-    console.log("ho gya itni jaldi")
+        let error = true
+        if(!error){
+            resolve({username: "javaScript", password: "1234"})
+        } else {
+            reject('ERROR, something went wrong')
+        }
+    },1000)
 })
+
+
+async function consumePromiseFive(){
+
+    try {
+        const response = await promiseFive()
+        console.log(response)
+    } catch (Errror){
+        console.log(Errror)
+    }
+}
+
+consumePromiseFive()
